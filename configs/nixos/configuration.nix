4ba5dd7be51993e23af10bf717a16dd2c588217c
Bas Nijholt <bas@nijho.lt>
2025-10-05 23:26:48 -0700

    simplify paths

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -6,22 +6,22 @@
 {
   imports = [
     ./kinto.nix
-    (builtins.path { path = ./modules/boot.nix; name = "boot.nix"; })
-    (builtins.path { path = ./modules/storage.nix; name = "storage.nix"; })
-    (builtins.path { path = ./modules/networking.nix; name = "networking.nix"; })
-    (builtins.path { path = ./modules/gaming.nix; name = "gaming.nix"; })
-    (builtins.path { path = ./modules/system-core.nix; name = "system-core.nix"; })
-    (builtins.path { path = ./modules/nix.nix; name = "nix.nix"; })
-    (builtins.path { path = ./modules/nixpkgs.nix; name = "nixpkgs.nix"; })
-    (builtins.path { path = ./modules/user.nix; name = "user.nix"; })
-    (builtins.path { path = ./modules/desktop.nix; name = "desktop.nix"; })
-    (builtins.path { path = ./modules/nvidia-graphics.nix; name = "nvidia-graphics.nix"; })
-    (builtins.path { path = ./modules/ai.nix; name = "ai.nix"; })
-    (builtins.path { path = ./modules/backup.nix; name = "backup.nix"; })
-    (builtins.path { path = ./modules/services.nix; name = "services.nix"; })
-    (builtins.path { path = ./modules/slurm.nix; name = "slurm.nix"; })
-    (builtins.path { path = ./modules/system-packages.nix; name = "system-packages.nix"; })
-    (builtins.path { path = ./modules/home-manager.nix; name = "home-manager.nix"; })
+    ./modules/boot.nix
+    ./modules/storage.nix
+    ./modules/networking.nix
+    ./modules/gaming.nix
+    ./modules/system-core.nix
+    ./modules/nix.nix
+    ./modules/nixpkgs.nix
+    ./modules/user.nix
+    ./modules/desktop.nix
+    ./modules/nvidia-graphics.nix
+    ./modules/ai.nix
+    ./modules/backup.nix
+    ./modules/services.nix
+    ./modules/slurm.nix
+    ./modules/system-packages.nix
+    ./modules/home-manager.nix
     ./hardware-configuration.nix
   ];
   # The system state version is critical and should match the installed NixOS release.
