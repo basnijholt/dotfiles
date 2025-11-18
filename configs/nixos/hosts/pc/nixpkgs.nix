33aa404d345dfd47a2b1cce466a59410a3cac8b7
Bas Nijholt <bas@nijho.lt>
2025-11-18 15:38:21 -0800

    Optimize build for llama.cpp

--- configs/nixos/hosts/pc/nixpkgs.nix
+++ configs/nixos/hosts/pc/nixpkgs.nix
@@ -30,20 +30,38 @@
           cudaSupport = true;
           rocmSupport = false;
           metalSupport = false;
+          # Enable BLAS for optimized CPU layer performance (OpenBLAS)
+          # This is crucial for models using split-mode or CPU offloading
+          blasSupport = true;
         }).overrideAttrs
           (oldAttrs: rec {
-            version = "7077";
+            version = "7097";
             src = pkgs.fetchFromGitHub {
               owner = "ggml-org";
               repo = "llama.cpp";
               tag = "b${version}";
-              hash = "sha256-cZgiIVbqrEKjcdGfCCwOT/6xPsBklt8PneHnilJVz/g=";
+              hash = "sha256-RmHpnbz4GAphlkqM5owpVeaRCTwKk6/zVG/rwFeKYoo=";
               leaveDotGit = true;
               postFetch = ''
                 git -C "$out" rev-parse --short HEAD > $out/COMMIT
                 find "$out" -name .git -print0 | xargs -0 rm -rf
               '';
             };
+            # Enable native CPU optimizations for massively better CPU performance
+            # This enables AVX, AVX2, AVX-512, FMA, etc. for your specific CPU
+            # NOTE: This is intentionally opposite of nixpkgs (which uses -DGGML_NATIVE=off
+            # for reproducible builds). We sacrifice portability for faster CPU layers.
+            cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+              "-DGGML_NATIVE=ON"
+            ];
+
+            # Disable Nix's NIX_ENFORCE_NO_NATIVE which strips -march=native flags
+            # See: https://github.com/NixOS/nixpkgs/issues/357736
+            # See: https://github.com/NixOS/nixpkgs/pull/377484 (intentionally contradicts this)
+            preConfigure = ''
+              export NIX_ENFORCE_NO_NATIVE=0
+              ${oldAttrs.preConfigure or ""}
+            '';
           });
 
       # llama-swap from GitHub releases
