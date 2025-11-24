9935ec9659118a0ecbaba8e048692805d3746560
Bas Nijholt <bas@nijho.lt>
2025-11-23 22:39:57 -0800

    Disable STP on NUC br0 to prevent DHCP timeouts

--- configs/nixos/hosts/nuc/networking.nix
+++ configs/nixos/hosts/nuc/networking.nix
@@ -10,11 +10,15 @@
   networking.bridges = {
     "br0" = {
       interfaces = [ "eno1" ];
+      stp = false; # Disable STP to avoid startup delays/timeouts on consumer routers
     };
   };
   
   networking.interfaces.br0.useDHCP = true;
+  
+  # Physical interface is a bridge member, no IP
   networking.interfaces.eno1.useDHCP = false;
+  networking.interfaces.eno1.ipv4.addresses = [];
 
   # Trust the bridge so VMs can do DHCP/DNS
   networking.firewall.trustedInterfaces = [ "br0" "incusbr0" ];
