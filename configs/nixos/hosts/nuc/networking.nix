60a47d77d32ec4affe3ebcc46d619029b720d74a
Bas Nijholt <bas@nijho.lt>
2025-11-23 22:21:29 -0800

    Configure br0 bridge on NUC for Incus VM networking

--- configs/nixos/hosts/nuc/networking.nix
+++ configs/nixos/hosts/nuc/networking.nix
@@ -4,8 +4,21 @@
   networking.hostName = "nuc";
   networking.nftables.enable = true;
   networking.firewall.enable = true;
-  networking.useDHCP = true;
   networking.networkmanager.enable = false;
+  
+  # Configure Bridge for Incus VMs/Containers
+  networking.bridges = {
+    "br0" = {
+      interfaces = [ "eno1" ];
+    };
+  };
+  
+  networking.interfaces.br0.useDHCP = true;
+  networking.interfaces.eno1.useDHCP = false;
+
+  # Trust the bridge so VMs can do DHCP/DNS
+  networking.firewall.trustedInterfaces = [ "br0" ];
+
   networking.firewall.allowedTCPPorts = [ 8080 ]; # Kodi web interface
   networking.firewall.allowedUDPPortRanges = [
     { from = 60000; to = 61000; } # mosh
