3a882243d012eeba11aefd2ec23ebf757076c162
Bas Nijholt <bas@nijho.lt>
2025-11-24 10:20:22 -0800

    Update HP networking to systemd-networkd with correct MAC

--- configs/nixos/hosts/hp/networking.nix
+++ configs/nixos/hosts/hp/networking.nix
@@ -5,16 +5,32 @@
   networking.nftables.enable = true;
   networking.firewall.enable = true;
   networking.networkmanager.enable = false;
-  
-  # Configure Bridge for Incus VMs/Containers
-  networking.bridges = {
-    "br0" = {
-      interfaces = [ "eno1" ];
+
+  # --- Systemd-Networkd Configuration (Robust Bridging) ---
+  networking.useDHCP = false; # Disable legacy scripted networking
+  systemd.network.enable = true;
+
+  # 1. Create the bridge device
+  systemd.network.netdevs."20-br0" = {
+    netdevConfig = {
+      Kind = "bridge";
+      Name = "br0";
+      MACAddress = "c8:d9:d2:0c:e0:34"; # Mimic eno1 MAC for Static DHCP
     };
   };
-  
-  networking.interfaces.br0.useDHCP = true;
-  networking.interfaces.eno1.useDHCP = false;
+
+  # 2. Bind physical interface to bridge
+  systemd.network.networks."30-eno1" = {
+    matchConfig.Name = "eno1";
+    networkConfig.Bridge = "br0";
+    linkConfig.RequiredForOnline = "no"; # Avoid boot hangs if cable issue
+  };
+
+  # 3. Configure the bridge (DHCP)
+  systemd.network.networks."40-br0" = {
+    matchConfig.Name = "br0";
+    networkConfig.DHCP = "yes";
+  };
 
   # Trust the bridge so VMs can do DHCP/DNS
   networking.firewall.trustedInterfaces = [ "br0" "incusbr0" ];
