1e454cbc51d648a62c7fbe48859c93168947baa8
Bas Nijholt <bas@nijho.lt>
2025-11-26 14:38:55 -0800

    Convert nuc to ZFS (#11)

--- configs/nixos/hosts/nuc/disko.nix
+++ configs/nixos/hosts/nuc/disko.nix
@@ -1,62 +1,73 @@
 { lib, ... }:
 
-let
-  mountOpts = [
-    "compress=zstd"
-    "noatime"
-    "discard=async"
-    "space_cache=v2"
-  ];
-in
 {
   disko.devices = {
-    disk.nvme = {
-      type = "disk";
-      device = "/dev/disk/by-id/nvme-CT4000P3PSSD8_2344E884093A";
-      content = {
-        type = "gpt";
-        partitions = {
-          esp = {
-            label = "EFI-NUC";
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
-            };
-          };
-
-          root = {
-            label = "ROOT-NUC";
-            size = "100%";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" "-L" "NUC-BTRFS" ];
-              subvolumes = {
-                "@root" = {
-                  mountpoint = "/";
-                  mountOptions = mountOpts;
-                };
-                "@nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = mountOpts;
-                };
-                "@var" = {
-                  mountpoint = "/var";
-                  mountOptions = mountOpts;
-                };
-                "@home" = {
-                  mountpoint = "/home";
-                  mountOptions = mountOpts;
-                };
-                "@snapshots" = {
-                  mountpoint = "/.snapshots";
-                  mountOptions = mountOpts;
-                };
+    disk = {
+      nvme = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-CT4000P3PSSD8_2344E884093A";
+        content = {
+          type = "gpt";
+          partitions = {
+            esp = {
+              label = "EFI-NUC";
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
+            root = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+
+    zpool = {
+      zroot = {
+        type = "zpool";
+        mode = ""; # Single disk
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+          acltype = "posixacl";
+          xattr = "sa";
+          atime = "off";
+        };
+
+        datasets = {
+          root = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options.mountpoint = "legacy";
+          };
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options.mountpoint = "legacy";
+          };
+          var = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+            options.mountpoint = "legacy";
+          };
+          home = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options.mountpoint = "legacy";
           };
         };
       };
