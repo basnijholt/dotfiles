fa890fa22ee608be2d11ab4f705550337092c414
Bas Nijholt <bas@nijho.lt>
2025-11-29 20:13:50 -0800

    nix: Fix CUDA binary cache configuration
    - Added official 'https://cache.nixos-cuda.org' to substituters.
    - Added public key 'cache.nixos-cuda.org:74DU...' (without -1 suffix).
    - Reference: https://wiki.nixos.org/wiki/CUDA

--- configs/nixos/common/nix.nix
+++ configs/nixos/common/nix.nix
@@ -14,7 +14,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "build-vm-1:CQeZikX76TXVMm+EXHMIj26lmmLqfSxv8wxOkwqBb3g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixos-cuda.org-1:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 }
