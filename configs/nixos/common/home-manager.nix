623e8b73c7186f036f32bb6df55009264420f590
Bas Nijholt <bas@nijho.lt>
2025-12-03 09:26:25 -0800

    Remove npm home treatment from nix home-manager (is in dotbot now)

--- configs/nixos/common/home-manager.nix
+++ configs/nixos/common/home-manager.nix
@@ -12,14 +12,6 @@
     {
       home.stateVersion = "25.05";
 
-      # Tell npm to install "global" packages into ~/.local/npm
-      home.file.".npmrc".text = ''
-        prefix=${config.home.homeDirectory}/.local/npm
-      '';
-
-      # Ensure ~/.local/npm/bin is on PATH for your sessions and user services
-      home.sessionPath = [ "${config.home.homeDirectory}/.local/npm/bin" ];
-
       # Create the directory at activate time (nice-to-have)
       home.activation.ensureNpmGlobalDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         mkdir -p "${config.home.homeDirectory}/.local/npm"
