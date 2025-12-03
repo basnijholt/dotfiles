0c3c5ca46da5ecd9ee6a56ce71060184149084c7
Bas Nijholt <bas@nijho.lt>
2025-12-03 07:19:51 -0800

    Use `~/.local/npm/bin` over `~/.npm-global`

--- configs/nixos/common/home-manager.nix
+++ configs/nixos/common/home-manager.nix
@@ -12,17 +12,17 @@
     {
       home.stateVersion = "25.05";
 
-      # Tell npm to install "global" packages into ~/.npm-global
+      # Tell npm to install "global" packages into ~/.local/npm
       home.file.".npmrc".text = ''
-        prefix=${config.home.homeDirectory}/.npm-global
+        prefix=${config.home.homeDirectory}/.local/npm
       '';
 
-      # Ensure ~/.npm-global/bin is on PATH for your sessions and user services
-      home.sessionPath = [ "${config.home.homeDirectory}/.npm-global/bin" ];
+      # Ensure ~/.local/npm/bin is on PATH for your sessions and user services
+      home.sessionPath = [ "${config.home.homeDirectory}/.local/npm/bin" ];
 
       # Create the directory at activate time (nice-to-have)
       home.activation.ensureNpmGlobalDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        mkdir -p "${config.home.homeDirectory}/.npm-global"
+        mkdir -p "${config.home.homeDirectory}/.local/npm"
       '';
 
     };
