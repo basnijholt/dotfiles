c8b8cf6dbcda9f1602b9d825dec49bea14013f04
Bas Nijholt <bas@nijho.lt>
2025-12-03 12:40:15 -0800

    fix(home-manager): skip private 'secrets' submodule on clone
    Clone without --recurse-submodules, then manually init submodules
    excluding 'secrets' which is a private repo requiring SSH auth.
    User can manually clone secrets later once SSH keys are set up.

--- configs/nixos/common/home-manager.nix
+++ configs/nixos/common/home-manager.nix
@@ -24,18 +24,19 @@
           export PATH="${pkgs.git}/bin:$PATH"
           # Initialize LFS hooks
           run ${pkgs.git-lfs}/bin/git-lfs install
-          # Clone without LFS files first (fast), fetch them after
+          # Clone without LFS files and without submodules first
           export GIT_LFS_SKIP_SMUDGE=1
           run ${pkgs.git}/bin/git \
             -c url."https://github.com/".insteadOf="git@github.com:" \
             clone \
             --depth 1 \
-            --shallow-submodules \
-            --recurse-submodules \
-            --jobs=8 \
             https://github.com/basnijholt/dotfiles.git \
             "${config.home.homeDirectory}/dotfiles"
-          # Then pull LFS files (in main repo and all submodules)
+          # Init submodules excluding 'secrets' (private repo, needs SSH key)
+          run cd "${config.home.homeDirectory}/dotfiles" && ${pkgs.git}/bin/git \
+            -c url."https://github.com/".insteadOf="git@github.com:" \
+            submodule update --init --recursive --depth 1 --jobs=8 -- ':!secrets'
+          # Pull LFS files (in main repo and all submodules)
           run cd "${config.home.homeDirectory}/dotfiles" && ${pkgs.git-lfs}/bin/git-lfs pull
           run cd "${config.home.homeDirectory}/dotfiles" && ${pkgs.git}/bin/git submodule foreach --recursive ${pkgs.git-lfs}/bin/git-lfs pull
         fi
