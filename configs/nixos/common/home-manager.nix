fc1f7539a39f3c0045a534819b88d30a8e8305ba
Bas Nijholt <bas@nijho.lt>
2025-12-03 13:15:15 -0800

    refactor(home-manager): simplify dotfiles setup (YAGNI)

--- configs/nixos/common/home-manager.nix
+++ configs/nixos/common/home-manager.nix
@@ -3,59 +3,29 @@
 
 {
   home-manager.users.basnijholt =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
+    { config, lib, pkgs, ... }:
     {
       home.stateVersion = "25.05";
 
-      # Create the directory at activate time (nice-to-have)
       home.activation.ensureNpmGlobalDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         mkdir -p "${config.home.homeDirectory}/.local/npm"
       '';
 
-      # Clone dotfiles repo if not present (uses HTTPS to avoid SSH key requirement)
-      home.activation.cloneDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        # Check for dotbot submodule to detect incomplete clones
-        if [ ! -d "${config.home.homeDirectory}/dotfiles/submodules/dotbot" ]; then
-          # Remove incomplete clone if it exists
-          run rm -rf "${config.home.homeDirectory}/dotfiles"
-          # Ensure git and git-lfs are in PATH
-          export PATH="${pkgs.git}/bin:${pkgs.git-lfs}/bin:$PATH"
-          # Initialize LFS hooks
+      # Clone dotfiles and run dotbot on first nixos-rebuild
+      home.activation.setupDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        export PATH="${pkgs.git}/bin:${pkgs.git-lfs}/bin:${pkgs.python3}/bin:$PATH"
+        if [ ! -d "${config.home.homeDirectory}/dotfiles/.git" ]; then
           run ${pkgs.git-lfs}/bin/git-lfs install
-          # Clone without LFS files and without submodules first
           export GIT_LFS_SKIP_SMUDGE=1
-          run ${pkgs.git}/bin/git \
-            -c url."https://github.com/".insteadOf="git@github.com:" \
-            clone \
-            --depth 1 \
-            https://github.com/basnijholt/dotfiles.git \
-            "${config.home.homeDirectory}/dotfiles"
-          # Pull LFS files in main repo first
+          run ${pkgs.git}/bin/git -c url."https://github.com/".insteadOf="git@github.com:" \
+            clone --depth 1 https://github.com/basnijholt/dotfiles.git "${config.home.homeDirectory}/dotfiles"
           unset GIT_LFS_SKIP_SMUDGE
           run cd "${config.home.homeDirectory}/dotfiles" && ${pkgs.git-lfs}/bin/git-lfs pull
-          # Init submodules excluding 'secrets' (private repo, needs SSH key)
-          # LFS files will be fetched during checkout now that SKIP_SMUDGE is unset
           run cd "${config.home.homeDirectory}/dotfiles" && ${pkgs.git}/bin/git \
             -c url."https://github.com/".insteadOf="git@github.com:" \
             submodule update --init --recursive --depth 1 --jobs=8 -- ':!secrets'
         fi
+        run ${config.home.homeDirectory}/dotfiles/install || true
       '';
-
-      # Run dotbot to symlink dotfiles
-      home.activation.runDotbot = lib.hm.dag.entryAfter [ "cloneDotfiles" ] ''
-        if [ -d "${config.home.homeDirectory}/dotfiles/submodules/dotbot" ]; then
-          # Ensure python3 and zsh are in PATH for dotbot
-          export PATH="${pkgs.python3}/bin:${pkgs.zsh}/bin:${pkgs.git}/bin:$PATH"
-          run ${config.home.homeDirectory}/dotfiles/install || true
-        else
-          echo "Skipping dotbot: dotfiles not fully cloned"
-        fi
-      '';
-
     };
 }
