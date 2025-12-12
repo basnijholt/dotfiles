c5a07f0220cee4608782bfe5802a666aeb42a317
Bas Nijholt <bas@nijho.lt>
2025-12-11 22:15:38 -0800

    fix: use reproducible derivation for get-apple-firmware

--- configs/nixos/hosts/macbook-air-intel/default.nix
+++ configs/nixos/hosts/macbook-air-intel/default.nix
@@ -31,9 +31,20 @@
   '';
 
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "get-apple-firmware" ''
-      echo "Downloading and running firmware extraction script..."
-      ${pkgs.curl}/bin/curl -fsSL https://raw.githubusercontent.com/t2linux/wiki/master/docs/firmware.sh | bash
-    '')
+    (pkgs.stdenvNoCC.mkDerivation {
+      pname = "get-apple-firmware";
+      version = "360156db52c03dbdac0ef9d6e2cebbca46b955b";
+      src = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/t2linux/wiki/360156db52c03dbdac0ef9d6e2cebbca46b955b/docs/tools/firmware.sh";
+        hash = "sha256-IL7omNdXROG402N2K9JfweretTnQujY67wKKC8JgxBo=";
+      };
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/bin
+        cp $src $out/bin/get-apple-firmware
+        chmod +x $out/bin/get-apple-firmware
+      '';
+      meta.mainProgram = "get-apple-firmware";
+    })
   ];
 }
