67703852c8ea6fff199a997d1673a1b64166b239
Bas Nijholt <bas@nijho.lt>
2025-12-21 21:57:02 -0800

    move rclone-b2-backup.nix to hosts/docker-lxc

--- configs/nixos/hosts/docker-lxc/default.nix
+++ configs/nixos/hosts/docker-lxc/default.nix
@@ -5,7 +5,7 @@
     ../dev-lxc/default.nix
     ../dev-lxc/hardware-configuration.nix
     ./packages.nix
-    ../../optional/rclone-b2-backup.nix
+    ./rclone-b2-backup.nix
   ];
 
   networking.hostName = lib.mkForce "docker-lxc";
