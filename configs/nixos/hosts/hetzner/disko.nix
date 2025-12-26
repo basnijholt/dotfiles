e3b9fd8e5934c80e827fcb85147772ff7ca40fe6
Bas Nijholt <bas@nijho.lt>
2025-12-26 00:28:48 -0800

    hetzner: add websites ZFS dataset to disko config
    Dedicated dataset for websites with:
    - lz4 compression
    - atime disabled
    - auto-snapshot enabled

--- configs/nixos/hosts/hetzner/disko.nix
+++ configs/nixos/hosts/hetzner/disko.nix
@@ -2,7 +2,24 @@
 #
 # Uses common ZFS disko config with Hetzner-specific label.
 # Hetzner exposes the disk as /dev/sda.
-(import ../../common/disko-zfs.nix) {
-  device = "/dev/sda";
-  espLabel = "ESP-HETZNER";
+{ lib, ... }:
+let
+  common = (import ../../common/disko-zfs.nix) {
+    device = "/dev/sda";
+    espLabel = "ESP-HETZNER";
+  };
+in
+{
+  disko.devices = lib.recursiveUpdate common.disko.devices {
+    zpool.zroot.datasets.websites = {
+      type = "zfs_fs";
+      mountpoint = "/home/basnijholt/websites";
+      options = {
+        mountpoint = "legacy";
+        compression = "lz4";
+        atime = "off";
+        "com.sun:auto-snapshot" = "true";
+      };
+    };
+  };
 }
