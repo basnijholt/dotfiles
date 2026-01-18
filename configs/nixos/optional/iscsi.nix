ff5d73e40c47d85252f6dc9802101d990eba511a
Bas Nijholt <bas@nijho.lt>
2026-01-18 00:48:06 -0800

    fix(iscsi): use IP instead of truenas.local
    Avoid mDNS resolution issues by using the static IP directly.

--- configs/nixos/optional/iscsi.nix
+++ configs/nixos/optional/iscsi.nix
@@ -1,8 +1,8 @@
 # iSCSI initiator for connecting to TrueNAS LUNs
 #
 # Manual usage (if not using auto-login):
-#   iscsiadm -m discovery -t sendtargets -p truenas.local
-#   iscsiadm -m node -T iqn.2005-10.org.freenas.ctl:jbweston1 -p truenas.local --login
+#   iscsiadm -m discovery -t sendtargets -p 192.168.1.4
+#   iscsiadm -m node -T iqn.2005-10.org.freenas.ctl:jbweston1 -p 192.168.1.4 --login
 #   lsblk
 { config, pkgs, ... }:
 
@@ -10,7 +10,7 @@
   services.openiscsi = {
     enable = true;
     name = "iqn.2024-01.org.nixos:${config.networking.hostName}";
-    discoverPortal = "truenas.local";
+    discoverPortal = "192.168.1.4"; # truenas
     enableAutoLoginOut = true;
   };
 
