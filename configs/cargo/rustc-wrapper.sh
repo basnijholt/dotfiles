fa5aef65980cff730714a6e932dd4929ae8ca991
Bas Nijholt <bas@nijho.lt>
2026-03-04 13:16:14 -0800

    cargo: make rustc-wrapper shell-portable

--- configs/cargo/rustc-wrapper.sh
+++ configs/cargo/rustc-wrapper.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env sh
 # Wrapper that uses sccache if available, otherwise falls back to rustc
-if command -v sccache &> /dev/null; then
-    exec sccache "$@"
+if command -v sccache >/dev/null 2>&1; then
+	exec sccache "$@"
 else
-    exec rustc "$@"
+	exec "$@"
 fi
