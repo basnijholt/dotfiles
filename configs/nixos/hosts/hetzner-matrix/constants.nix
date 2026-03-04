6deaaa5f836d8abafa4c212512784f81b0c5c210
Bas Nijholt <bas@nijho.lt>
2026-03-04 12:48:46 -0800

    chore(hetzner-matrix): bump tuwunel to v1.5.0-mindroom.9

--- configs/nixos/hosts/hetzner-matrix/constants.nix
+++ configs/nixos/hosts/hetzner-matrix/constants.nix
@@ -1,6 +1,6 @@
 {
   siteDomain = "mindroom.chat";
   cinnyDomain = "chat.mindroom.chat";
-  tuwunelVersion = "v1.5.0-mindroom.2";
-  tuwunelArchiveHash = "sha256-NLgt+LB7NeO/d3deAqf/jx2KhyB34SaFtszFI9X81/8=";
+  tuwunelVersion = "v1.5.0-mindroom.9";
+  tuwunelArchiveHash = "sha256-egzFGLX4hh7df+Q0u+Zy/oyX6rNoZdetUuy6ISs6dxs=";
 }
