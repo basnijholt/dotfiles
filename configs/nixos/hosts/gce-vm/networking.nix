e5c28cc2bf28baf02bfb00abbd27f44104ae2b79
Bas Nijholt <bas@nijho.lt>
2026-08-16 16:08:36 -0700

    fix(nixos): enable firewall on GCE

--- configs/nixos/hosts/gce-vm/networking.nix
+++ configs/nixos/hosts/gce-vm/networking.nix
@@ -5,6 +5,8 @@
     hostName = lib.mkForce "gce-vm";
     useDHCP = lib.mkDefault true;
     nameservers = lib.mkForce [ "169.254.169.254" ];
+    nftables.enable = true;
+    firewall.enable = true;
     firewall.interfaces.tailscale0.allowedTCPPorts = [ 3773 ];
   };
 }
